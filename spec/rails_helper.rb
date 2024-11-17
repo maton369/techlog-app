c3fa6cbe6d5fb116edde9d4969d1b7399b2f7e63
ysk901085@gmail.com <maton@DESKTOP-F2NH3CT>
2024-11-17 11:01:39 +0900

    ナビゲーションバーを追加

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -17,4 +17,6 @@ RSpec.configure do |config|
   config.filter_rails_from_backtrace!
 
   config.include FactoryBot::Syntax::Methods
+
+  config.include Devise::Test::IntegrationHelpers, type: :system
 end
