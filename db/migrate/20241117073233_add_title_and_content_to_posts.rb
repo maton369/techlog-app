8a45dc75a19523e538a04ee126ff81877f4b2f22
ysk901085@gmail.com <maton@DESKTOP-F2NH3CT>
2024-11-17 18:10:28 +0900

    バグ取り

--- db/migrate/20241117073233_add_title_and_content_to_posts.rb
+++
@@ -1,6 +0,0 @@
-class AddTitleAndContentToPosts < ActiveRecord::Migration[7.2]
-  def change
-    add_column :posts, :title, :string
-    add_column :posts, :content, :string
-  end
-end
--- db/migrate/20241117090943_create_posts.rb
+++ db/migrate/20241117090943_create_posts.rb
@@ -1,9 +1,10 @@
 class CreatePosts < ActiveRecord::Migration[7.2]
   def change
     create_table :posts do |t|
-      t.string :title, null: false
-      t.string :content, null: false
-      t.references :user, foreign_key: true, null: false
+      t.string :title
+      t.text :content
+      t.references :user, null: false, foreign_key: true
+
       t.timestamps
     end
   end
